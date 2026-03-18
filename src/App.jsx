2a01fa2c626cbdf5af126f4b461135315b89b26e
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-03-18 11:09:04 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,47 @@
+import logo from "./assets/Zwap_logo_full.png";
+
 export default function App() {
   return (
-    <div style={{ background: "black", color: "white", minHeight: "100vh", padding: "40px" }}>
-      <h1>ZWAP</h1>
-      <p>Move. Play. Swap. Shop.</p>
+    <div
+      style={{
+        background: "black",
+        color: "white",
+        minHeight: "100vh",
+        padding: "40px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <img
+        src={logo}
+        alt="ZWAP logo"
+        style={{ width: "220px", marginBottom: "20px" }}
+      />
+
+      <p style={{ fontSize: "20px", marginTop: "10px", opacity: 0.8 }}>
+        Move. Play. Swap. Shop.
+      </p>
+
+      <p style={{ marginTop: "20px", maxWidth: "420px", opacity: 0.65 }}>
+        Most apps waste your time. ZWAP turns it into rewards.
+      </p>
+
+      <button
+        style={{
+          marginTop: "30px",
+          padding: "15px 25px",
+          fontSize: "16px",
+          background: "white",
+          color: "black",
+          border: "none",
+          borderRadius: "10px",
+          cursor: "pointer",
+          width: "fit-content",
+        }}
+      >
+        Enter App
+      </button>
     </div>
   );
 }
