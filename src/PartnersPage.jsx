e4eaf04ec0d041af4ad239f5f66cd1a731bd5e65
XOCLON HOLDINGS INC. <devin@xoclonholdings.property>
2026-03-30 08:20:53 -0400

    Update PartnersPage.jsx

--- src/PartnersPage.jsx
+++ src/PartnersPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
+import SponsorsForm from "./SponsorsForm";
 
 function GradientText({ children }) {
   return (
