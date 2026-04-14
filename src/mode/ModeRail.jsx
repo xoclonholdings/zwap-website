d15850613a5ce825d0d18cb06d8f06111c4845b4
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-14 08:32:22 -0400

    Create ModeRail.jsx

---
+++ src/mode/ModeRail.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ModeButton from "./ModeButton";
+
+export default function ModeRail({
+  modes = [],
+  activeMode,
+  setActiveMode,
+  direction = "vertical",
+}) {
+  const isVertical = direction === "vertical";
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: isVertical ? "column" : "row",
+        gap: isVertical ? 12 : 10,
+        justifyContent: isVertical ? "center" : "space-between",
+        alignItems: "stretch",
+        width: "100%",
+        height: isVertical ? "100%" : "auto",
+        flexWrap: isVertical ? "nowrap" : "nowrap",
+      }}
+    >
+      {modes.map((mode) => (
+        <ModeButton
+          key={mode.id}
+          label={mode.label}
+          active={activeMode === mode.id}
+          onClick={() => setActiveMode(mode.id)}
+          fullWidth={!isVertical}
+        />
+      ))}
+    </div>
+  );
+}
