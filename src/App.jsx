47f73578761dfa0b8b4666c4653020c7800c35aa
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-15 05:03:10 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import bang from "./assets/Zwap_bang_3d.png";
-import LandingHeader from "./LandingHeader";
-import EarlyAccessModal from "./EarlyAccessModal.jsx";
-import PreviewPage from "./preview/PreviewPage";
+
+import LandingHeader from "./components/LandingHeader";
+import EarlyAccessModal from "./components/EarlyAccessModal";
+import MailingListDatabase from "./components/MailingListDatabase";
 
 import AboutPage from "./pages/AboutPage";
 import FeaturesPage from "./pages/FeaturesPage";
@@ -12,11 +13,12 @@ import TermsPage from "./pages/TermsPage";
 import GooglePlay from "./pages/GooglePlay";
 import AppleStore from "./pages/AppleStore";
 
-import MailingListDatabase from "./MailingListDatabase";
+import PreviewPage from "./preview/PreviewPage";
+
 import MailModal from "./app/MailModal";
 import AppHome from "./app/AppHome";
 import AppFooter from "./app/AppFooter";
-import PreviewUnlockRedirect from "./app/PreviewUnlockRedirect";
+import AppRoutes from "./app/AppRoutes";
 import appStyles from "./app/appStyles";
 
 import useAppState from "./app/useAppState";
@@ -82,104 +84,34 @@ export default function App() {
     openEarlyAccessModal,
   });
 
-  if (activePage === "about") {
-    return (
-      <>
-        <AboutPage
-          onBack={() => setActivePage("home")}
-          onLockIn={() => openEarlyAccessModal("preview")}
-        />
+  const routedPage = (
+    <AppRoutes
+      activePage={activePage}
+      setActivePage={setActivePage}
+      previewUnlocked={previewUnlocked}
+      openEarlyAccessModal={openEarlyAccessModal}
+      closeEarlyAccessModal={closeEarlyAccessModal}
+      unlockPreview={unlockPreview}
+      isModalOpen={isModalOpen}
+      email={email}
+      setEmail={setEmail}
+      bang={bang}
+      pendingPage={pendingPage}
+      AboutPage={AboutPage}
+      FeaturesPage={FeaturesPage}
+      PartnersPage={PartnersPage}
+      PrivacyPage={PrivacyPage}
+      TermsPage={TermsPage}
+      GooglePlay={GooglePlay}
+      AppleStore={AppleStore}
+      MailingListDatabase={MailingListDatabase}
+      PreviewPage={PreviewPage}
+      EarlyAccessModal={EarlyAccessModal}
+    />
+  );
 
-        <EarlyAccessModal
-          isOpen={isModalOpen}
-          onClose={closeEarlyAccessModal}
-          email={email}
-          setEmail={setEmail}
-          bang={bang}
-          onSubmitAccess={unlockPreview}
-          onInstantAccess={unlockPreview}
-          pendingPage={pendingPage}
-        />
-      </>
-    );
-  }
-
-  if (activePage === "features") {
-    return <FeaturesPage onBack={() => setActivePage("home")} />;
-  }
-
-  if (activePage === "partners") {
-    return <PartnersPage onBack={() => setActivePage("home")} />;
-  }
-
-  if (activePage === "privacy") {
-    return <PrivacyPage />;
-  }
-
-  if (activePage === "terms") {
-    return <TermsPage />;
-  }
-
-  if (activePage === "google-play") {
-    return <GooglePlay />;
-  }
-
-  if (activePage === "apple-store") {
-    return <AppleStore />;
-  }
-
-  if (activePage === "mailing-list") {
-    return (
-      <MailingListDatabase
-        onBack={() => setActivePage("home")}
-        email={email}
-        setEmail={setEmail}
-        onSubmitAccess={unlockPreview}
-      />
-    );
-  }
-
-  if (activePage === "preview") {
-    if (!previewUnlocked) {
-      return (
-        <>
-          <PreviewUnlockRedirect
-            onOpen={() => openEarlyAccessModal("preview")}
-          />
-
-          <EarlyAccessModal
-            isOpen={isModalOpen}
-            onClose={closeEarlyAccessModal}
-            email={email}
-            setEmail={setEmail}
-            bang={bang}
-            onSubmitAccess={unlockPreview}
-            onInstantAccess={unlockPreview}
-            pendingPage={pendingPage}
-          />
-        </>
-      );
-    }
-
-    return (
-      <>
-        <PreviewPage
-          onBack={() => setActivePage("home")}
-          onLockIn={() => openEarlyAccessModal("preview")}
-        />
-
-        <EarlyAccessModal
-          isOpen={isModalOpen}
-          onClose={closeEarlyAccessModal}
-          email={email}
-          setEmail={setEmail}
-          bang={bang}
-          onSubmitAccess={unlockPreview}
-          onInstantAccess={unlockPreview}
-          pendingPage={pendingPage}
-        />
-      </>
-    );
+  if (routedPage) {
+    return routedPage;
   }
 
   return (
