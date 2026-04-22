2bfae9e4d80106608619e5a2e01deebbad251193
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-22 14:50:58 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { prerender } from "vite-plugin-prerender-static";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: [
-        "/",
-        "/about",
-        "/news",
-        "/blog",
-        "/features",
-        "/partners",
-        "/developers",
-        "/enterprise",
-        "/contact",
-        "/faq",
-        "/how-it-works",
-        "/privacy",
-        "/terms",
-        "/sitemap"
-      ]
-    })
-  ]
+  plugins: [react()],
 });
