8b841091e97dc046597e8b8ca3dce71b3fdbe563
XOCLON HOLDINGS INC. <214429976+xoclonholdings@users.noreply.github.com>
2026-04-23 10:54:02 -0400

    Create public-get-posts.js

---
+++ netlify/functions/public-get-posts.js
@@ -0,0 +1,41 @@
+const { getDb } = require("./utils/mongo");
+
+exports.handler = async function handler(event) {
+  if (event.httpMethod !== "GET") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: "Method not allowed" }),
+    };
+  }
+
+  try {
+    const db = await getDb();
+
+    const posts = await db
+      .collection("website_posts")
+      .find({
+        status: "published",
+        deletedAt: { $in: [null, ""] },
+      })
+      .sort({ createdAt: -1 })
+      .toArray();
+
+    const normalizedPosts = posts.map((post) => ({
+      ...post,
+      _id: post._id.toString(),
+    }));
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(normalizedPosts),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: "Failed to load public posts",
+        details: error.message,
+      }),
+    };
+  }
+};
